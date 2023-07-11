947c14a6b38a9b98bf860ac44dafe4cd94ae828d
IrynaKazmirchuk <irakazm@gmail.com>
2023-07-11 18:27:53 +0300

    init video

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function navToggle() {
-    document.getElementById("headerMenu").classList.toggle("_active");
-    document.getElementById("burger").classList.toggle("_active");
+  document.getElementById("headerMenu").classList.toggle("_active");
+  document.getElementById("burger").classList.toggle("_active");
+}
+function TogglePlayPause() {
+  document.getElementById("video").classList.toggle("_active");
 }
